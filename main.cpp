5b4225a450ba44e11804e893694d87d2b19dfdae
Ciekce <kantaja98@gmail.com>
2023-05-16 12:10:16 +0100

    initial clang-format config, 4 space indents

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <cstdio>
 
 struct myStruct {
-  int x;
-  int y;
-  int z;
+    int x;
+    int y;
+    int z;
 };
 
 int main() {
-  printf("Hello World!\n");
+    printf("Hello World!\n");
 
-  int x = 0;
-  x = x + 1;
+    int x = 0;
+    x = x + 1;
 
-  myStruct s = {0, 1, 2};
-  printf("s.x = %d\n", s.x);
+    myStruct s = {0, 1, 2};
+    printf("s.x = %d\n", s.x);
 
-  // x = 1
-  printf("x = %d\n", x);
+    // x = 1
+    printf("x = %d\n", x);
 
-  return 0;
+    return 0;
 }
