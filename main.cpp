ca214bbe0275b2cdb20e6d026c990bfbfe3f0009
crippa1337 <casdcasd5@gmail.com>
2023-05-31 22:10:10 +0200

    use the right methods

--- main.cpp
+++ main.cpp
@@ -622,10 +622,10 @@ int main() {
         } else if (tokens[0] == "isready")
             std::cout << "readyok" << std::endl;
         else if (tokens[0] == "ucinewgame")
-            board = Board::fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            board.parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         else if (tokens[0] == "position") {
             // assume that the second token is 'startpos'
-            board = Board::fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            board.parseFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
             // minifier enable filter delete
             if (tokens[1] == "fen") {
