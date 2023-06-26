e2487f57c605d4380fe3d81e99ffd5d81da289ef
JacquesRW <jamie.whiting2003@gmail.com>
2023-06-26 12:13:09 +0100

    clang format ahhhhh

--- main.cpp
+++ main.cpp
@@ -661,7 +661,7 @@ int32_t negamax(auto &board, auto &threadData, auto ply, auto depth, auto alpha,
 
         alpha = max(alpha, staticEval);
     } else if (ply > 0 && board.history.size() > 1)
-        for (const auto &i: board.history)
+        for (const auto &i : board.history)
             if (board.state.hash == i.hash) return 0;
 
     auto i = 0;
